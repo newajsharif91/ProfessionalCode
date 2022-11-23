5fcede2ad6297505c9aeebf9fe106a2f41b7accd
“[newajsharif91]” <[email>
2022-11-23 19:03:51 +0600

    perfectly done

--- StudentList.java
+++ StudentList.java
@@ -8,12 +8,14 @@ public class StudentList {
 		if(args[0].equals("a")) {
 			System.out.println("Loading data ...");			
 			try {
-			BufferedReader s = new BufferedReader(
+			    BufferedReader s = new BufferedReader(
 					new InputStreamReader(
 							new FileInputStream("students.txt"))); 
-			String r = s.readLine();
-			String i[] = r.split(",");			
-			for(String j : i) { System.out.println(j); }
+			    String r = s.readLine();
+			    String i[] = r.split(",");			
+			    for(String j : i) {
+			    System.out.println(j);
+		        }
 			} catch (Exception e){} 
 			System.out.println("Data Loaded.");
 		}
@@ -53,19 +55,19 @@ public class StudentList {
 		{
 			System.out.println("Loading data ...");			
 			try {
-			BufferedReader s = new BufferedReader(
+			    BufferedReader s = new BufferedReader(
 					new InputStreamReader(
 							new FileInputStream("students.txt"))); 
-			String r = s.readLine();
-			String i[] = r.split(",");	
-			boolean done = false;
-			String t = args[0].substring(1);
-			for(int idx = 0; idx<i.length && !done; idx++) {
-				if(i[idx].equals(t)) {
-					System.out.println("We found it!");
+			    String r = s.readLine();
+			    String i[] = r.split(",");	
+			    boolean done = false;
+			    String t = args[0].substring(1);
+			    for(int idx = 0; idx<i.length && !done; idx++){
+				    if(i[idx].equals(t)) {
+					    System.out.println("We found it!");
 						done=true;
-				}
-			}
+				    }
+			    }
 			} catch (Exception e){} 
 			System.out.println("Data Loaded.");				
 		}
@@ -73,18 +75,21 @@ public class StudentList {
 		{
 			System.out.println("Loading data ...");			
 			try {
-			BufferedReader s = new BufferedReader(
+			    BufferedReader s = new BufferedReader(
 					new InputStreamReader(
 							new FileInputStream("students.txt"))); 
-			String D = s.readLine();
-			char a[] = D.toCharArray();			
-			boolean in_word = false;
-			int count=0;
-			for(char c:a) {
-				if(c ==' ') 
-				{
-					if (!in_word) {	count++; in_word =true;	}
-					else { in_word=false;}			
+			    String D = s.readLine();
+			    char a[] = D.toCharArray();			
+			    boolean in_word = false;
+			    int count=0;
+			    for(char c:a) {
+				if(c ==' ') {
+					if (!in_word) {
+						count++; in_word =true;	
+					}
+					else { 
+					in_word=false;
+					}			
 				}
 			}
 			System.out.println(count +" word(s) found " + a.length);
